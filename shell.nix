4fe0344a98e8e110c5161a0cadca881fb1f4e7bf
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-01-24 08:09:59 +0500

    feat: init

---
+++ shell.nix
@@ -0,0 +1,17 @@
+# https://github.com/shakhzodkudratov/nix-shell-workspace
+{ pkgs ? let 
+  nixpkgs-unstable = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  };
+  in import nixpkgs-unstable {}
+}: pkgs.mkShell {
+  buildInputs = [
+    pkgs.go
+    pkgs.ginkgo
+  ];
+
+  GOROOT = "${pkgs.go}/share/go";
+  GOBIN = "${pkgs.go}/share/go/bin";
+  # https://discourse.nixos.org/t/golang-delve-debugger-with-nix-flake-issue/22740
+  hardeningDisable = [ "all" ];
+}
